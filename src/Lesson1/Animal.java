a16a896607130961d186faa449413c1c3f9a78bd
Dmitrii Elkin <yolkindm@mail.ru>
2023-04-18 08:10:04 +0300

    first commit

---
+++ src/Lesson1/Animal.java
@@ -0,0 +1,5 @@
+package Lesson1;
+
+public abstract class Animal {
+    public abstract void tellAboutYOurself();
+}
---
+++ src/Lesson1/Main.java
@@ -0,0 +1,17 @@
+package Lesson1;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.setName("barsik");
+        cat.setAge(4);
+        cat.setInGoodMood(true);
+        Dog dog = new Dog();
+        dog.setName("sharik");
+        dog.setAge(4);
+        dog.setBreed("ovcharka");
+        cat.tellAboutYOurself();
+        dog.tellAboutYOurself();
+
+    }
+}
